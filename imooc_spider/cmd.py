87d81b11a3f6fe61db68a004de2ae676fef1b399
hellofjh <fengjunhao30@gmail.com>
2020-11-11 17:37:57 +0800

    rm all

--- imooc_spider/cmd.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-from scrapy import cmdline
-# 方式一：注意execute的参数类型为一个列表
-cmdline.execute('scrapy crawl imooc'.split())
-# cmdline.execute('scrapy crawl imooc -o items.json'.split())
